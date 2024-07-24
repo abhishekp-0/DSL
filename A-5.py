86c7152e75ddc215d1b5bb0332eb808a769b56bb
abhishekp-0 <132842566+abhishekp-0@users.noreply.github.com>
2024-07-24 10:07:46 +0530

    Add files via upload

--- A-5.py
+++ A-5.py
@@ -5,11 +5,27 @@ def linearSearch(l,n):
         if(l[i]==n):
             print("key found after ", count , " comparisons at position ", i+1, " with Linear Search")
             return i
-    print("Key not found with Linear Search!!")
+    print("Key not found with Linear Search!! Comparisions done : " ,count)
     return -1    
+
+def sentinelSearch(a,key):
+    count = 0
+    i=0
+    a.append(key)
+    while(key!=a[i]):
+        count+=1
+        i+=1
+    a.pop()
+    if(i<len(a)):
+        print("key found after ", count , " comparisons at position ", i+1, " with Sentinel Search")
+        return i
+    else:
+        print("Key not found with Sentinel Search!! Comparisions done : " ,count)
+        return -1 
     
 def binarySearch(key,a,f,l):
     count=0
+    a.sort()
     while(f<=l):
         count+=1
         mid=int((f+l)/2)
@@ -20,22 +36,22 @@ def binarySearch(key,a,f,l):
             f=mid+1
         elif(a[mid]>key):
             l=mid-1
-    print("Key not found with Binary Search!!")
+    print("Key not found with Binary Search!! Comparisions done : " ,count)
     return -1
-           
+
+a=[]
+n=int(input("Enter no.of elements in array"))
+for i in range(n):
+    x=int(input("Enter element"))
+    a.append(x)        
 
 flag=1
 while (flag==1):
-    a=[]
-    n=int(input("Enter no.of elements in array"))
-    for i in range(n):
-        x=int(input("Enter element"))
-        a.append(x)
     
     key=int(input("Enter element to be searched : "))
-    
     linearSearch(a,key)
     binarySearch(key,a,0,len(a)-1)
+    sentinelSearch(a,key)
     r=input("Do you want to continue (y/n) : ")
     if(r=="y"):
         flag=1
