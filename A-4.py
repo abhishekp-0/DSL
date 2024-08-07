6d16f0d4c8af26844e2082ef83d11fb5dc9a5319
abhishekp-0 <132842566+abhishekp-0@users.noreply.github.com>
2024-08-07 10:43:08 +0530

    Add files via upload

--- A-4.py
+++ A-4.py
@@ -1,4 +1,7 @@
-def TransposeSparseMatrix(sp_r,cl,rw,nz_el) :
+def TransposeSparseMatrix(sp_r,sp) :
+    rw=len(sp)
+    cl=len(sp[0])
+    nz_el=sp_r[0][2]
     print("Transpose of sparse matrix: ")
     trans_sp=[0]*(nz_el+1)
     ct=[0]*cl
@@ -15,9 +18,93 @@ def TransposeSparseMatrix(sp_r,cl,rw,nz_el) :
     for i in range(0,nz_el+1):
         print(trans_sp[i])
 
+def SimpleTranspose(sp_r):
+    for i in range(sp_r[0][2]+1):
+        temp=sp_r[i]
+    rw=sp_r[0][0]
+    cl=sp_r[0][1]
+    trans_sp=[]
+    nz_el=sp_r[0][2]
+    sp_r=[]
+    sp_r.append([cl,rw,nz_el])
+
+    for i in range(cl):
+        x=[]
+        for j in range(rw):
+            x.append(0)
+        trans_sp.append(x)
+
+    for i in range(cl):
+        for j in range(rw):
+            trans_sp[i][j]=sp[j][i]
+            if(trans_sp[i][j]!=0):
+                sp_r.append([i,j,trans_sp[i][j]])
+    
+    return sp_r
+
+def AddSparse(sp1,sp2):
+    r1=sp1[0][0]
+    c1=sp1[0][1]
+    n1=sp1[0][2]+1
+    r2=sp2[0][0]
+    c2=sp2[0][1]
+    n2=sp2[0][2]+1
+    sp3=[]
+    sp3.append([r1,c1,0])
+    if(r1!=r2 or c1!=c2):
+        print("Given matrix cannot be added")
+        return sp3
+
+    i=1
+    j=1
+    while(i<n1 and j<n2):
+        if(sp1[i][0]==sp2[j][0] and sp1[i][1]==sp2[j][1]):
+            sp3.append([sp1[i][0],sp1[i][1],sp1[i][2]+sp2[j][2]])
+            sp3[0][2]+=1
+            i+=1
+            j+=1
+        elif(sp1[i][0]==sp2[j][0]):
+            if(sp1[i][1]<sp2[j][1]):
+                sp3.append(sp1[i])
+                sp3.append(sp2[j])
+            else:
+                sp3.append(sp2[j])
+                sp3.append(sp1[i])
+            i+=1
+            j+=1
+            sp3[0][2]+=2
+
+        else:
+            if(sp1[i][0]<sp2[j][0]):
+                sp3.append(sp1[i])
+                i+=1
+            else:
+                sp3.append(sp2[j])
+                j+=1
+            sp3[0][2]+=1
+            
+    
+    while(i<n1):
+        sp3.append(sp1[i])
+        i+=1
+        sp3[0][2]+=1
+
+    while(j<n2):
+        sp3.append(sp2[j])
+        j+=1
+        sp3[0][2]+=1
+
+    
+    return sp3
+            
+
+
+    
+
 
 rw=int(input("Enter no. of Rows : "))
 cl=int(input("Enter no. of Columns : "))
+
 sp=[]
 nz_el=0
 sp_r=[]
@@ -39,4 +126,9 @@ print("sparse matrix is :")
 for i in range(0,nz_el+1):
     print(sp_r[i])
 
-TransposeSparseMatrix(sp_r,cl,rw,nz_el)
+sp2=[[3,3,4],[0,0,1],[1,1,1],[1,2,1],[2,1,1]]
+
+print(AddSparse(sp_r,sp2))
+TransposeSparseMatrix(sp_r,sp)
+print(SimpleTranspose(sp,sp_r))
+
